bc6d0be9db464dbafb5442a78a7caf68758470ec
aditd5 <aditdhiantono@gmail.com>
2023-06-06 12:36:54 +0700

    Add firebase auth

--- app/src/main/java/com/aditd5/localshop/SignInActivity.kt
+++ app/src/main/java/com/aditd5/localshop/SignInActivity.kt
@@ -1,11 +1,24 @@
 package com.aditd5.localshop
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aditd5.localshop.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth : FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignInBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignupsi.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+        }
     }
 }
--- app/src/main/java/com/aditd5/localshop/SignUpActivity.kt
+++ app/src/main/java/com/aditd5/localshop/SignUpActivity.kt
@@ -2,10 +2,43 @@ package com.aditd5.localshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import com.aditd5.localshop.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnSignup.setOnClickListener {
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPasswordsu.text.toString()
+
+            if (email.isEmpty()) {
+                binding.etEmail.error = "Email harus diisi"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.etEmail.error = "Email tidak valid"
+                binding.etEmail.requestFocus()
+                return@setOnClickListener
+            }
+
+            if (password.isEmpty()) {
+                binding.etPasswordsu.error = "Password harus diisi"
+                binding.etPasswordsu.requestFocus()
+                return@setOnClickListener
+            }
+
+        }
     }
 }
